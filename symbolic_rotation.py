fc4d8c5a5d66b2b19f188c5c7f4a1cc14a78fefb
VitorQuintans <vitor.quintans@grad.ufsc.br>
2024-04-23 04:16:47 +0200

    feat: adding inverse kinematics

--- symbolic_rotation.py
+++ symbolic_rotation.py
@@ -1,5 +1,6 @@
 # %% Importing libraries
 import sympy as sp
+import pydantic as pyd
 
 
 # %% Defining rotation matrices
@@ -44,7 +45,9 @@ def Rz(alpha, radians=True):
 
 
 # %% Homogenous transformation matrix
-def homogenous_matrix(R: sp.Matrix, t: sp.Matrix) -> sp.Matrix:
+def homogenous_matrix(
+    Rot: sp.Matrix = sp.Identity(3), trans: sp.Matrix = [0, 0, 0]
+) -> sp.Matrix:
     """Homogenous transformation matrix.
 
     Args:
@@ -56,16 +59,16 @@ def homogenous_matrix(R: sp.Matrix, t: sp.Matrix) -> sp.Matrix:
     """
     return sp.Matrix(
         [
-            [R[0, 0], R[0, 1], R[0, 2], t[0]],
-            [R[1, 0], R[1, 1], R[1, 2], t[1]],
-            [R[2, 0], R[2, 1], R[2, 2], t[2]],
+            [Rot[0, 0], Rot[0, 1], Rot[0, 2], trans[0]],
+            [Rot[1, 0], Rot[1, 1], Rot[1, 2], trans[1]],
+            [Rot[2, 0], Rot[2, 1], Rot[2, 2], trans[2]],
             [0, 0, 0, 1],
         ]
     )
 
 
 # %% Testing matrix multiplication
-# TODO: Abstract this to a function
+
 denavit_hartenberg = [
     {"theta": sp.symbols("theta1"), "d": 0, "a": 0, "alpha": -sp.pi / 2},
     {"theta": sp.symbols("theta2"), "d": sp.symbols("d2"), "a": 0, "alpha": sp.pi / 2},
@@ -85,6 +88,91 @@ for i, a in enumerate(A):
         continue
     A_result = A_result * a
 sp.simplify(A_result)
+
+
+# %% Denavid-Hartenberg class
+class DenavitHartenbergParams(pyd.BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
+    theta: sp.Symbol | float
+    d: sp.Symbol | float
+    a: sp.Symbol | float
+    alpha: sp.Symbol | float
+
+
+class FromDenavidHatenberg:
+    def __init__(self, dh_parameters: list[DenavitHartenbergParams]):
+        self.dh_parameters = dh_parameters
+        self.A, self.T_all = self.get_homogenous_matrix()
+
+    def get_homogenous_matrix(self):
+        A = []
+        for i, dh_parameters in enumerate(self.dh_parameters):
+            theta, d, a, alpha = dh_parameters.values()
+            print(f"DH parameters for line {i+1}: {theta, d, a, alpha}")
+            A.append(
+                homogenous_matrix(Rz(theta), sp.Matrix([0, 0, d]))
+                * homogenous_matrix(Rx(alpha), sp.Matrix([a, 0, 0]))
+            )
+        T_all = A[0]
+        for i, a in enumerate(A):
+            if i == 0:
+                continue
+            T_all = T_all * a
+        return A, sp.simplify(T_all)
+
+
+# %% Spherical manipulator
+spherical_dh_params: list[DenavitHartenbergParams] = [
+    {"theta": sp.symbols("theta1"), "d": 0, "a": 0, "alpha": -sp.pi / 2},
+    {"theta": sp.symbols("theta2"), "d": sp.symbols("d2"), "a": 0, "alpha": sp.pi / 2},
+    {"theta": 0, "d": sp.symbols("d3"), "a": 0, "alpha": 0},
+]
+spherical_manipulator = FromDenavidHatenberg(spherical_dh_params)
+spherical_manipulator.T_all
+
+# %% Spherical wrist
+
+spherical_wrist_dh_params: list[DenavitHartenbergParams] = [
+    {"theta": sp.symbols("theta1"), "d": 0, "a": 0, "alpha": -sp.pi / 2},
+    {"theta": sp.symbols("theta2"), "d": 0, "a": 0, "alpha": sp.pi / 2},
+    {"theta": sp.symbols("theta3"), "d": 0, "a": 0, "alpha": 0},
+]
+spherical_wrist = FromDenavidHatenberg(spherical_wrist_dh_params)
+spherical_wrist.T_all
+
+# %% Inverse kinematics
+phi = 45
+theta = 0
+psi = 90
+R = Rz(phi, radians=False) * Ry(theta, radians=False) * Rx(psi, radians=False)
+R_homogeneus = homogenous_matrix(Rot=R)
+
+spherical_wrist.T_all
+
+
+# FIXME system solving not working
+def solve_inverse_kinematics(
+    manipulator_matrix: sp.Matrix, desired_matrix: sp.Matrix, n_equations=3
+):
+    zero_matrix = manipulator_matrix - desired_matrix
+    print(zero_matrix)
+    return sp.nsolve(
+        (
+            zero_matrix[2, 0],
+            zero_matrix[2, 1],
+            zero_matrix[2, 2],
+            zero_matrix[3, 0],
+            zero_matrix[3, 1],
+            zero_matrix[3, 2],
+        ),
+        (sp.symbols("theta1"), sp.symbols("theta2"), sp.symbols("theta3")),
+        (0, 0, 0),
+    )
+
+
+solve_inverse_kinematics(spherical_wrist.T_all, R_homogeneus)
 # theta, d, a, alpha = sp.symbols("theta d a alpha")
 # homogenous_matrix(Rz(theta), sp.Matrix([0, 0, d])) * homogenous_matrix(
 # Rx(alpha), sp.Matrix([a, 0, 0])
